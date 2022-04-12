8d361f8494748f3d1d4083af58bc4ff3ac07dc0a
Brilliant Kwakye <kwakyebrilliant@gmail.com>
2022-04-12 13:07:25 +0000

    Update commit

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
+import { FormControl, Button } from "react-bootstrap";
+import * as IoIcons from 'react-icons/io';
 
 class Explore extends Component {
 
 
   render() {
     return (
-      <div className='p-5'>
-        <h1>Explore</h1>
-        <h1>Explore</h1>
-        <h1>Explore</h1>
-        <h1>Explore</h1>
+        <div className="container-fluid bg-white mt-5">
+            <div className="row">
+            <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '700px' }}>
+            <div className="content mr-auto ml-auto">
+            <div className="col-lg-12 p-3 m-auto mb-3">
+                <div className="d-flex">
+                <FormControl
+                    type="search"
+                    placeholder="Search"
+                    className="me-2"
+                    aria-label="Search"
+                    required
+                />
+                <Button
+                    variant="outline-success"
+                >
+                    <IoIcons.IoMdSearch size={23} />
+                </Button>
+                </div>
+            </div>
+
+                    <div className="col-lg-12 mb-4 mt-5" >
+                    <div className="card-header">
+
+                    </div>
+                    </div>
+            </div>
+            </main>
+            </div>
       </div>
     );
   }
--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -5,12 +5,20 @@ class Notifications extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Notifications</h1>
-        <h1>Notifications</h1>
-        <h1>Notifications</h1>
-        <h1>Notifications</h1>
+      <div className="container-fluid bg-white mt-5">
+      <div className="row">
+      <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '900px' }}>
+      <div className="content mr-auto ml-auto">
+
+              <div className="col-lg-12 mb-4 mt-5" >
+              <div className="card-header">
+
+              </div>
+              </div>
       </div>
+      </main>
+      </div>
+</div>
     );
   }
 }
